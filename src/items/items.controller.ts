b1631a6a72db7daeb74d6a768e9903893ae5ed79
loveisgala <gkozakievich@gmail.com>
2022-08-02 13:48:59 -0300

    Prettier fix

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 
-@Controller('items')
+@Controller('items') // http://localhost:3000/items
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';

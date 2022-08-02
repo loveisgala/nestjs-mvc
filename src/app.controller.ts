5bcdb72321994eb04b94f021db2c985df3c60aea
loveisgala <gkozakievich@gmail.com>
2022-08-02 13:44:03 -0300

    Create items resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// references http://localhost:3000
 @Controller()
 export class AppController {
+  // service: AppService
   constructor(private readonly appService: AppService) {}
+
+  // http://localhost:3000/users
+  @Get('users')
+  getUsers(): string[] {
+    // getUsers() will be created at app.service.ts
+    return this.appService.getUsers();
+  }
 
   @Get()
   getHello(): string {
